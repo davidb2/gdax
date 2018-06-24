fd754746692211d5d274975465425d9c56fc3863
= <brewcrew5498@outlook.com>
2018-06-24 17:03:41 -0400

    Run go fmt on all files.

--- gdax/coinbase_accounts.go
+++ gdax/coinbase_accounts.go
@@ -1,32 +1,32 @@
 package gdax
 
 import (
-	"net/http"
 	"github.com/google/uuid"
+	"net/http"
 	// "github.com/mitchellh/mapstructure"
 )
 
 type CoinbaseAccount struct {
-	Id *uuid.UUID `json:"id,string"`
-	Name string `json:"name"`
-	Balance float64 `json:"balance,string"`
-	Currency string `json:"currency"`
-	Type string `json:"wallet"`
-	Primary bool `json:"primary"`
-	Active bool `json:"active"`
+	Id       *uuid.UUID `json:"id,string"`
+	Name     string     `json:"name"`
+	Balance  float64    `json:"balance,string"`
+	Currency string     `json:"currency"`
+	Type     string     `json:"wallet"`
+	Primary  bool       `json:"primary"`
+	Active   bool       `json:"active"`
 }
 
 type CoinbaseAccountCollection struct {
 	pageableCollection
 	requestSent bool
-	pages [][]CoinbaseAccount
+	pages       [][]CoinbaseAccount
 }
 
 func (accessInfo *AccessInfo) GetCoinbaseAccounts() *CoinbaseAccountCollection {
 	coinbaseAccountCollection := CoinbaseAccountCollection{
 		pageableCollection: accessInfo.newPageableCollection(false),
-		requestSent: false,
-		pages: nil,
+		requestSent:        false,
+		pages:              nil,
 	}
 	return &coinbaseAccountCollection
 }
--- gdax/types.go
+++ gdax/types.go
@@ -1,11 +1,11 @@
 package gdax
 
 import (
-  "reflect"
-  "time"
-  "encoding/json"
-  "strings"
-  "fmt"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
 )
 
 type pageable interface {
@@ -15,47 +15,47 @@ type pageable interface {
 
 type pagination struct {
 	before string
-	after string
-	limit int
+	after  string
+	limit  int
 }
 
 type pageableCollection struct {
-	currentPage int
+	currentPage        int
 	currentIndexInPage int
-	accessInfo *AccessInfo
+	accessInfo         *AccessInfo
 	pagination
-	size int
-  finishedReadingPage bool
-  pendingError error
-  usesPaginationCursors bool
-  pages [][]interface{}
+	size                  int
+	finishedReadingPage   bool
+	pendingError          error
+	usesPaginationCursors bool
+	pages                 [][]interface{}
 }
 
 type DayHourMin struct {
-  time.Time
+	time.Time
 }
 
 func (d *DayHourMin) MarshalJSON() ([]byte, error) {
-  return []byte(d.Format("mm,hh,dd")), nil
+	return []byte(d.Format("mm,hh,dd")), nil
 }
 
 func (accessInfo *AccessInfo) newPageableCollection(usesPaginationCursors bool) pageableCollection {
-  return pageableCollection{
-    currentPage: -1,
-    accessInfo: accessInfo,
-    pagination: pagination{before: "", after: "", limit: -1},
-    size: 0,
-    finishedReadingPage: false,
-    pendingError: nil,
-    usesPaginationCursors: usesPaginationCursors,
-    pages: nil,
-  }
+	return pageableCollection{
+		currentPage:           -1,
+		accessInfo:            accessInfo,
+		pagination:            pagination{before: "", after: "", limit: -1},
+		size:                  0,
+		finishedReadingPage:   false,
+		pendingError:          nil,
+		usesPaginationCursors: usesPaginationCursors,
+		pages: nil,
+	}
 }
 func (p pagination) String() string {
 	var (
 		before string
-		after string
-		limit string
+		after  string
+		limit  string
 	)
 	if p.before != "" {
 		before = fmt.Sprintf("before=%s", p.before)
@@ -70,49 +70,49 @@ func (p pagination) String() string {
 }
 
 func (c *pageableCollection) hasNext(method, path, params, body string, container interface{}) bool {
-  if len(c.pages) == 0 || c.currentIndexInPage == len(c.pages[c.currentPage]) {
-      if len(c.pages) > 0 && !c.usesPaginationCursors {
-        return false
-      }
-      c.finishedReadingPage = true
-  } else if !c.finishedReadingPage {
-    return true
-  }
+	if len(c.pages) == 0 || c.currentIndexInPage == len(c.pages[c.currentPage]) {
+		if len(c.pages) > 0 && !c.usesPaginationCursors {
+			return false
+		}
+		c.finishedReadingPage = true
+	} else if !c.finishedReadingPage {
+		return true
+	}
 
-  respBody, cursor, err := c.accessInfo.collectionRequest(method, fmt.Sprintf("%s?%s&%s", path, params, c.pagination), body)
-  if err != nil {
-    c.pendingError = err
-    return true
-  }
-  err = json.Unmarshal([]byte(respBody), &container)
-  if err != nil {
-    c.pendingError = err
-    return true
-  }
-  c.pagination = *cursor
-  page := reflect.ValueOf(container).Elem()
-  if page.Len() == 0 {
-    return false
-  }
-  z := make([]interface{}, page.Len())
-  for i := 0; i < page.Len(); i++ {
-    z[i] = page.Index(i)
-  }
-  var y [][]interface{}
-  a := reflect.ValueOf(reflect.ValueOf(c.pages).Interface())
-  c.pages = reflect.Append(a, reflect.ValueOf(z)).Convert(reflect.TypeOf(y)).Interface().([][]interface{})
-  c.size++
-  c.currentIndexInPage = 0
-  c.currentPage++
-  c.finishedReadingPage = false
-  return true
+	respBody, cursor, err := c.accessInfo.collectionRequest(method, fmt.Sprintf("%s?%s&%s", path, params, c.pagination), body)
+	if err != nil {
+		c.pendingError = err
+		return true
+	}
+	err = json.Unmarshal([]byte(respBody), &container)
+	if err != nil {
+		c.pendingError = err
+		return true
+	}
+	c.pagination = *cursor
+	page := reflect.ValueOf(container).Elem()
+	if page.Len() == 0 {
+		return false
+	}
+	z := make([]interface{}, page.Len())
+	for i := 0; i < page.Len(); i++ {
+		z[i] = page.Index(i)
+	}
+	var y [][]interface{}
+	a := reflect.ValueOf(reflect.ValueOf(c.pages).Interface())
+	c.pages = reflect.Append(a, reflect.ValueOf(z)).Convert(reflect.TypeOf(y)).Interface().([][]interface{})
+	c.size++
+	c.currentIndexInPage = 0
+	c.currentPage++
+	c.finishedReadingPage = false
+	return true
 }
 
 func (c *pageableCollection) next() (reflect.Value, error) {
-  if c.pendingError != nil {
-    return reflect.ValueOf(nil), c.pendingError
-  }
-  elem := c.pages[c.currentPage][c.currentIndexInPage]
-  c.currentIndexInPage++
-  return elem.(reflect.Value), nil
+	if c.pendingError != nil {
+		return reflect.ValueOf(nil), c.pendingError
+	}
+	elem := c.pages[c.currentPage][c.currentIndexInPage]
+	c.currentIndexInPage++
+	return elem.(reflect.Value), nil
 }
